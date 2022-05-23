13b26f52860e2766adb18e8333c049d7c2e09e74
midgjs <sjqksk.10@gmail.com>
2022-05-23 16:43:45 +0900

    book 기초

--- src/com/javaex/ex01/BookSelectAll.java
+++ src/com/javaex/ex01/BookSelectAll.java
@@ -3,48 +3,57 @@ package com.javaex.ex01;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class Authorinsert {
+public class BookSelectAll {
 
 	public static void main(String[] args) {
-		
-		
 		// 0. import java.sql.*;
 		Connection conn = null;
 		PreparedStatement pstmt = null;
-		//ResultSet rs = null; 셀렉트문에서 사용
+		ResultSet rs = null; //결과값 받기 위해 사용
 
 		try {
 		    // 1. JDBC 드라이버 (Oracle) 로딩
 			Class.forName("oracle.jdbc.driver.OracleDriver");
-			
+
 		    // 2. Connection 얻어오기
 			String url = "jdbc:oracle:thin:@localhost:1521:xe";
 			conn = DriverManager.getConnection(url, "webdb", "webdb");
 
 		    // 3. SQL문 준비 / 바인딩 / 실행
-			
-			//SQL문 준비
-			//String str = "insert into author values(seq_author_id.nextval, '김문열', '경북 양양')";
+		    //SQL문 준비
 			String query = "";
-			query += " insert into author ";
-			query += " values(seq_author_id.nextval, ?, ?) ";
-			System.out.println(query); //확인용
+			query += " select  book_id, ";
+			query += " 		   title, ";
+			query += " 		   pubs ";
+			query += " 		   to_char(pub_date, 'yyyy-mm-dd') pub_date, ";
+			query += " 		   author_id, ";
+			query += "         author_name, ";
+			query += "         author_desc ";
+			query += " from book b, author a ";
+			query += " where a.author_id = b.author_id ";
 			
 			//바인딩
-			pstmt = conn.prepareStatement(query); //문자열 쿼리로 만들기
-			pstmt.setString(1, "김영하");         //?(물음표) 중 1번째 -> 순서중요
-			pstmt.setString(2, "알쓸신잡");	      //?(물음표) 중 2번째 -> 순서중요
+			pstmt = conn.prepareStatement(query);
 			
 			//실행
-			int count = pstmt.executeUpdate();	  //쿼리문 실행 - 데이터 보내기 -> 성공갯수 retrun
-			
+			rs = pstmt.executeQuery();    //select
 			
 		    // 4.결과처리
-			System.out.println(count + "건이 등록 되었습니다.");
+			while (rs.next()) {
+				int bookId = rs.getInt(1);
+				String title = rs.getString(2);
+				String pubs = rs.getString(3);
+				String pubDate = rs.getString(4);
+				int aAuthorId = rs.getInt(5);
+				String authorName = rs.getString(6);
+				String authorDesc = rs.getString(7);
+				
+			System.out.println(bookId + ", " + title + ", " + pubs + ", " + pubDate + ", " + aAuthorId + ", " + authorName + ", " + authorDesc);
 			
-			
+			}
 			
 		} catch (ClassNotFoundException e) {
 		    System.out.println("error: 드라이버 로딩 실패 - " + e);
@@ -54,11 +63,9 @@ public class Authorinsert {
 		   
 		    // 5. 자원정리
 		    try {
-		    	/* 셀렉트문에서 사용
 		        if (rs != null) {
 		            rs.close();
-		        }
-		        */                
+		        } 
 		        if (pstmt != null) {
 		            pstmt.close();
 		        }
@@ -70,7 +77,7 @@ public class Authorinsert {
 		    }
 
 		}
-
+		
 
 	}
 
